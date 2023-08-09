e367557f37f98127205a681c26ba2b2cdfd15a8f
Rohith Ramanathan <rohram04@gmail.com>
2023-08-09 01:27:30 -0400

    fixing hover on mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 const plugin = require("tailwindcss/plugin");
 
 module.exports = {
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
